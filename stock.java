ad44fa415a09594fd13d0e8001c0a92dfda640c5
sallyshin <46617568+sallyshin@users.noreply.github.com>
2021-05-05 01:36:45 +0900

    adding stack manual
    스택에서 사용되는 메소드 메뉴얼 정리

--- stock.java
+++ stock.java
@@ -5,10 +5,14 @@ class stock {
         Stack<Integer> stack = new Stack<>();
         
         for(int idx = 0; idx < prices.length; idx++){
-            while(!stack.empty() && prices[idx] < prices[stack.peek()]){
+            //by 세정, 스택이 비어있는지 확인하는 boolean형 메소드 empty()
+            while(!stack.empty() && prices[idx] < prices[stack.peek()]){ // 생성한 stack에는 index를 넣어준다.
+                // by 세정, 스택의 맨위 요소를 확인하는 메소드 peek()
                 answer[stack.peek()] = idx - stack.peek();
+                // by 세정, 스택의 맨위 요소를 꺼내는 메소드 pop()
                 stack.pop();
             }
+            // by 세정, 스택의 맨위에 함수 인자로 받은 요소를 넣는 push(sth)
             stack.push(idx);
         }
         while(!stack.empty()){
