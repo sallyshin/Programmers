3bc356653ea618bf95984f6a1fe37596a67cda29
sallyshin <46617568+sallyshin@users.noreply.github.com>
2021-06-02 23:44:02 +0900

    Add files via upload
    프로그래머스 프린터
    https://programmers.co.kr/learn/courses/30/lessons/42587

---
+++ printer.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+class printer{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
