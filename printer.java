39a9c06c0d850b3df93946bf3ed01842a85f0360
sallyshin <46617568+sallyshin@users.noreply.github.com>
2021-06-03 00:28:12 +0900

    프린터
    https://programmers.co.kr/learn/courses/30/lessons/42587

--- printer.java
+++ printer.java
@@ -1,8 +1,39 @@
-import java.io.*;
+import java.util.*;
 
-class printer{
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+         Queue<Printer> que = new LinkedList<>();
         
+        for(int i = 0; i < priorities.length; i++){
+            que.offer(new Printer(i, priorities[i]));
+        }
+        while(!que.isEmpty()){
+            int tmp = que.peek().prior;
+            boolean check = false;
+            
+            for(Printer p : que){
+                if(tmp < p.prior){
+                    check = true;
+                }
+            }
+            if(check){
+                que.add(que.poll());
+            } else {
+                if(que.poll().location == location){
+                    answer = priorities.length - que.size();
+                }
+            }
+        }
+        return answer;
+    }
+    class Printer {
+        int location;
+        int prior;
+
+        Printer(int loc, int pri){
+            this.location = loc;
+            this.prior = pri;
+        }
     }
 }
